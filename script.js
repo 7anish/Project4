8076bbdf2bd804128dea16925360db38f1967ed8
Anish <anissh946@gmail.com>
2024-07-26 22:37:06 +0530

    Check the url by fetching url

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const addbtn = document.getElementById("add")
-addbtn.addEventListener('click', () => {
+addbtn.addEventListener('click', async () => {
     const url = document.getElementById("url").value;
-    if (checkurl(url)) {
+    const isvalidurl = await checkurl(url);
+    if (isvalidurl) {
         const arr = JSON.parse(localStorage.getItem("collection")) ?? [];
         arr.push(url)
         localStorage.setItem("collection", JSON.stringify(arr))
@@ -13,7 +14,6 @@ addbtn.addEventListener('click', () => {
         document.getElementById("url").value = "";
     }
 })
-
 
 // Refresh Image container whenever some change made
 const containerreset = () => {
@@ -32,7 +32,6 @@ const containerreset = () => {
     document.querySelector(".image-container").innerHTML = html
 }
 
-
 // Loading the localstaorage whe page opens
 const firstload = () => {
     containerreset()
@@ -50,9 +49,10 @@ const deleteitem = (i) => {
 
 
 // Edit button
-const edititme = (i) => {
+const edititme = async (i) => {
     const url = prompt("Enter new Url");
-    if (checkurl(url)) {
+    const isvalidurl = await checkurl(url);
+    if (isvalidurl) {
         const arr = JSON.parse(localStorage.getItem("collection"))
         arr.splice(i, 1, url);
         localStorage.setItem("collection", JSON.stringify(arr))
@@ -67,49 +67,51 @@ const edititme = (i) => {
 // const checkurl = (url) => {
 //     return (/(https?:\/\/.*\.(?:png|jpg|webp|jpeg))/i).test(url)
 // }
-
 // Checking by async and await function
 
-// const checkurl = async (url) => {
-//     if(url.trim() == ""){
-//         return false
-//     }
-//         try{
-//             const res = await fetch(url);
-//             console.log(res.status)
-//             if(res.status == 200){
-//                 console.log("Anish");
-//                 return true
-//             }
-//             else{
-//                 console.log("Anish1");
-//                 return false
-//             }
-//         }catch(e){
-//             console.log(e);
-//             return false
-//         }
-// }
+const checkurl = async (url) => {
+    if (url.trim() == "") {
+        return false
+    }
+    try {
+        const res = await fetch(url);
+        console.log(res.status)
+        if (res.status == 200) {
+            console.log("Anish");
+            return true
+        }
+        else {
+            console.log("Anish1");
+            return false
+        }
+    } catch (e) {
+        console.log(e);
+        return false
+    }
+}
 
 
 // checking imag is valid or not by creating an image object
 
-const checkurl = (url)=>{
-    const img = new Image();
-    img.src = url;
-    console.log(img.width)
-    if(img.width == 0){
-        return false
-    }
-    else{
-        return true
-    }
-}
-
+// const checkurl = (url)=>{
+//     const img = new Image();
+//     img.src = url;
+//     console.log(img.src)
+//     console.log(img.width)
+//     if(img.width== 0){
+//         console.log("false")
+//         return false
+//     }
+//     else{
+//         console.log("ture")
+//         return true
+//     }
+// }
 
 // scroll button
 const down = document.querySelector(".down")
 down.addEventListener('click', () => {
     window.scrollBy(0, window.innerHeight)
 })
+
 
